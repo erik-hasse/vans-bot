3942beae6f68aaad897d470248232ebb254a7c16
Erik Hasse <erik.g.hasse@gmail.com>
2023-12-11 16:21:02 -0600

    Improve parsing of lead times page

--- tests/test_lead_times.py
+++ tests/test_lead_times.py
@@ -15,28 +15,9 @@ def lead_time_page():
         return f.read()
 
 
-def test_get_price_update(
-    lead_time_monitor: VansLeadTimeMonitor, lead_time_page: bytes
-):
-    assert (
-        lead_time_monitor.get_current_price_update(lead_time_page)
-        == "March 6, 2023, 3:00 pm, Pacific Time"
-    )
-
-
-def test_get_lead_time_update(
-    lead_time_monitor: VansLeadTimeMonitor, lead_time_page: bytes
-):
-    assert (
-        lead_time_monitor.get_current_lead_time_update(lead_time_page) == "July 6, 2023"
-    )
-
-
 def test_page_structure_changed(lead_time_monitor: VansLeadTimeMonitor):
     with pytest.raises(ValueError):
-        lead_time_monitor.get_current_price_update(b"")
-    with pytest.raises(ValueError):
-        lead_time_monitor.get_current_lead_time_update(b"")
+        lead_time_monitor.parse_sections(b"")
 
     lead_time_monitor.get_current_lead_time_page = (  # type: ignore[method-assign]
         lambda: b""
@@ -46,17 +27,26 @@ def test_page_structure_changed(lead_time_monitor: VansLeadTimeMonitor):
     assert lead_time_monitor.check_for_messages() == []
 
 
+def test_parse_sections(lead_time_monitor: VansLeadTimeMonitor, lead_time_page: bytes):
+    sections = lead_time_monitor.parse_sections(lead_time_page)
+    assert len(sections) == 3
+    assert all(len(section) > 0 for section in sections.values())
+
+
 def test_check_for_messages(
     lead_time_monitor: VansLeadTimeMonitor, lead_time_page: bytes
 ):
-    lead_time_monitor.price_update = "January 1, 1970"
     lead_time_monitor.get_current_lead_time_page = (  # type: ignore[method-assign]
         lambda: lead_time_page
     )
+    lead_time_monitor.sections = lead_time_monitor.parse_sections(lead_time_page)
+    lead_time_monitor.sections["RV Kit Prices"] = "foo"
     assert lead_time_monitor.check_for_messages() == [
         (
-            "Van's has updated their <https://www.vansaircraft.com/order-a-kit/"
-            "kit-prices-and-lead-times/|kit prices and lead times>!"
+            "Van's has updated the <https://www.vansaircraft.com/order-a-kit/"
+            "kit-prices-and-lead-times/|kit prices and lead times> page!"
+            "The following sections have changed:\n"
+            "* RV Kit Prices"
         )
     ]
 
--- vans_bot/vans_lead_times.py
+++ vans_bot/vans_lead_times.py
@@ -1,15 +1,19 @@
-import re
+import logging
+import typing as t
+from itertools import takewhile
 
+import bs4
 import requests
 
 from vans_bot.base import BaseChecker
+
+logger = logging.getLogger(__name__)
 
 
 class VansLeadTimeMonitor(BaseChecker):
     def __init__(self):
         page = self.get_current_lead_time_page()
-        self.price_update = self.get_current_price_update(page)
-        self.lead_time_update = self.get_current_lead_time_update(page)
+        self.sections = self.parse_sections(page)
         self.continue_checking = True
 
     def get_current_lead_time_page(self) -> bytes:
@@ -18,39 +22,43 @@ class VansLeadTimeMonitor(BaseChecker):
         )
         return resp.content
 
-    def get_current_price_update(self, page: bytes) -> str:
-        r = re.search(rb"<em>Kit prices effective: (.+)</em>", page)
-        if r is None:
-            raise ValueError("Could not find price update - the page has changed")
-        return r.group(1).decode("utf-8")
+    def parse_sections(self, page: bytes) -> dict[str, t.Any]:
+        soup = bs4.BeautifulSoup(page, features="html.parser")
+        headings = soup.find_all("h3")
+        if len(headings) != 3 or len(set(h.parent for h in headings)) != 1:
+            raise ValueError("Page structure has changed!")
 
-    def get_current_lead_time_update(self, page: bytes) -> str:
-        r = re.search(rb"<em>Estimated lead times effective: (.+)</em>", page)
-        if r is None:
-            raise ValueError("Could not find lead time update - the page has changed")
-        return r.group(1).decode("utf-8")
+        return {
+            h.text.strip(" \n:"): list(
+                takewhile(lambda h: h.name != "h3", h.next_siblings)
+            )
+            for h in headings
+        }
 
     def check_for_messages(self) -> list[str]:
         if not self.continue_checking:
             return []
         page = self.get_current_lead_time_page()
         try:
-            price_update = self.get_current_price_update(page)
-            lead_time_update = self.get_current_lead_time_update(page)
-        except ValueError:
+            sections = self.parse_sections(page)
+        except ValueError as e:
+            logger.exception(e)
             self.continue_checking = False
             return [
-                "Van's has changed <https://www.vansaircraft.com/order-a-kit/"
-                "kit-prices-and-lead-times/|kit prices and lead times>!"
+                "Van's has changed the <https://www.vansaircraft.com/order-a-kit/"
+                "kit-prices-and-lead-times/|kit prices and lead times> page!"
             ]
-        if (
-            price_update != self.price_update
-            or lead_time_update != self.lead_time_update
-        ):
-            self.price_update = price_update
-            self.lead_time_update = lead_time_update
+        if sections != self.sections:
+            changed_sections = [
+                name
+                for name, section in sections.items()
+                if section != self.sections.get(name)
+            ]
+            self.sections = sections
             return [
-                "Van's has updated their <https://www.vansaircraft.com/order-a-kit/"
-                "kit-prices-and-lead-times/|kit prices and lead times>!"
+                "Van's has updated the <https://www.vansaircraft.com/order-a-kit/"
+                "kit-prices-and-lead-times/|kit prices and lead times> page!"
+                "The following sections have changed:\n"
+                + "\n".join(f"* {name}" for name in changed_sections)
             ]
         return []
